eb745eefda827b1062e734e2e3d2bfa07a0631a4
jkirouac42 <jkirouac42@gmail.com>
2016-07-28 10:47:40 -0400

    actual final

--- Magic8Ball/JavaScript.js
+++ Magic8Ball/JavaScript.js
@@ -1,24 +1,10 @@
-﻿function getRandom() {
+﻿
+var responses = ["Yes", "No", "Maybe", "Unclear try again later", "Wrong question"];
 
+//http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
 
-
- var responses = ["Yes", "No", "Maybe", "Unclear try again later", "Wrong question"];
-    var number=  Math.floor(Math.random() * 5) + 1;
-   // var rand = responses[Math.floor(Math.random() * responses.length)]
-       
-
-        if (number == 1) {
-            alert(responses[0]);
-        }
-        if (number == 2) {
-            alert(responses[1]);
-        }
-        if (number == 3) {
-            alert(responses[2]);
-        }
-        if (number == 4) {
-            alert(responses[3]);
-        }
-        else
-            alert(responses[4]);
-    }
+function getRandom() {
+    var rand = Math.floor(Math.random() * responses.length);
+    var concat = responses[rand];
+    document.getElementById("answer").innerHTML = (concat);
+}
